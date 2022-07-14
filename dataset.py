0c96f0a9d0dd39338be8dcd87cedd8ef520d77c2
hrishikeshps94 <hrishikeshps94@gmial.com>
2022-07-14 15:13:42 +0530

    new_update

--- dataset.py
+++ dataset.py
@@ -18,30 +18,33 @@ class Custom_Dataset(Dataset):
         self.is_train = is_train
         self.hq_im_file_list = []
         self.lq_im_file_list = []
+        self.hq_train_files = {}
+        self.lq_train_files = {}
         for dir_path, _, file_names in os.walk(root_dir):
             for f_paths in sorted(file_names):
-                if dir_path.endswith('HQ'):
+                if dir_path.endswith('GT'):
                     self.hq_im_file_list.append(os.path.join(dir_path,f_paths))
-                elif dir_path.endswith('LQ'):
+                elif dir_path.endswith('input'):
                     self.lq_im_file_list.append(os.path.join(dir_path,f_paths))
-        self.tensor_convert = T.ToTensor()
-        self.train_transform = T.Compose(
-    [T.RandomVerticalFlip(p=0.5),T.RandomHorizontalFlip(p=0.5)\
-       ,T.RandomAffine((0,360)),T.Resize((256,256)),T.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])])
-
-        self.val_transform = T.Compose([T.Resize((256,256)),T.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])])
+        for im_names in self.hq_im_file_list:
+            self.hq_train_files[im_names] = np.load(im_names)
+        for im_names in self.lq_im_file_list:
+            self.lq_train_files[im_names] = np.load(im_names)
+        self.train_transform = T.Compose([T.RandomCrop((512,512))])
     def __len__(self):
         return len(self.hq_im_file_list)
-
+    def tone_transform(self,im,c=0.25):
+      mapped_x = im / (im + c)
+      return mapped_x
     def __getitem__(self, idx):
-        image_hq_fname = self.hq_im_file_list[idx]
-        image_lq_fname = self.lq_im_file_list[idx]
-        hq_image = self.tensor_convert(Image.open(image_hq_fname).convert('RGB')).unsqueeze(dim=0)
-        lq_image = self.tensor_convert(Image.open(image_lq_fname).convert('RGB')).unsqueeze(dim=0)
+        image_hq_fname = self.hq_train_files[self.hq_im_file_list[idx]]
+        image_lq_fname = self.lq_train_files[self.lq_im_file_list[idx]]
+        hq_image = torch.from_numpy(self.tone_transform(image_hq_fname)).unsqueeze(dim=0).permute(0,3,1,2)
+        lq_image = torch.from_numpy(self.tone_transform(image_lq_fname)).unsqueeze(dim=0).permute(0,3,1,2)
         concat_img = torch.cat([hq_image,lq_image],dim=0)
         if self.is_train:
             image = self.train_transform(concat_img)
         else:
-            image = self.val_transform(concat_img)
+            image = concat_img
         hq_img,lq_img = image.tensor_split(2)
         return lq_img.squeeze(0),hq_img.squeeze(0)
